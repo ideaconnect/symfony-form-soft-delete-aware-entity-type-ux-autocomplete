ad5eec3313332833f03736ea0af9fdf329534857
Bartosz Pachołek <bartosz+github@idct.tech>
2024-07-16 23:52:08 +0200

    fixed run tests order

--- run-tests.sh
+++ run-tests.sh
@@ -5,8 +5,8 @@ cd tests/functional \
 && docker compose build \
 && docker compose up -d --build --force-recreate \
 && docker compose exec  --workdir=/app testapp composer install \
-&& docker compose exec  --workdir=/app/tests/functional testapp bin/console importmap:require tom-select/dist/css/tom-select.default.css \
 && docker compose exec  --workdir=/app/tests/functional testapp composer install \
+&& docker compose exec  --workdir=/app/tests/functional testapp bin/console importmap:require tom-select/dist/css/tom-select.default.css \
 && docker compose exec  --workdir=/app testapp ./vendor/bin/phpunit \
 && sleep 5 \
 && docker compose exec testapp bin/console doctrine:schema:create \
